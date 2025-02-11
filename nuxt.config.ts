b9228a7e60bfea3813551cdc78a8d90bbe6740a4
Sharmae09 <sharmae.code@gmail.com>
2025-02-11 14:11:54 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,10 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   compatibilityDate: "2024-11-07",
   ssr: false,
+  vue: {
+    compilerOptions: {
+      // This function tells Vue to treat any element that starts with "ion-" as a custom element
+      isCustomElement: (tag) => tag.startsWith("ion-")
+    }
+  }
 });
